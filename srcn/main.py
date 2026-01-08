fee3f656638e1e5c98abcb80e0a09591a7de7571
arfat-dev <ongkiresman@gmail.com>
2026-01-08 14:31:19 +0900

    Delete srcn/main.py

--- srcn/main.py
+++
@@ -1,9 +0,0 @@
-from buku import Buku
-from perpustakaan import Perpustakaan
-
-perpus = Perpustakaan()
-
-b1 = Buku("B001", "Python Dasar", "Guido")
-perpus.tambah_buku(b1)
-
-perpus.tampilkan_buku()
